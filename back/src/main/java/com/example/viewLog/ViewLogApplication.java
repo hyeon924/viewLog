d8ca909a8fe0d779a4ae38970163ad68c4de1fb5
hyeon924 <hyeonn4713@gmail.com>
2025-09-24 00:35:11 +0900

    back setting-01

---
+++ back/src/main/java/com/example/viewLog/ViewLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.viewLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewLogApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/viewLog/ViewLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viewLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
